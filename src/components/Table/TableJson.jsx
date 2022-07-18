f0134ee35751d6e05ea56aad069368bb4246ee87
mwyzer <muhammad.wyzer@gmail.com>
2022-07-18 15:39:13 +0700

    not passing data

--- src/components/Table/TableJson.jsx
+++ src/components/Table/TableJson.jsx
@@ -1,14 +1,148 @@
 import 'antd/dist/antd.css';
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
+import { Button, Input, Space, Table } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 import JsonData from '../../data/people.json';
 
-function TableJson() {
+const TableJson = () => {
   const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
 
-  const DisplayData = JsonData.map((info) => {
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+      >
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{
+              width: 90,
+            }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size='small'
+            style={{
+              width: 90,
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1890ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '30%',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'age',
+      width: '20%',
+      ...getColumnSearchProps('email'),
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      ...getColumnSearchProps('address'),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ['descend', 'ascend'],
+    },
+  ];
+
+  const DisplayData = JsonData?.map((info, index) => {
     return (
-      <tr>
-        <td>{info._id}</td>
+      <tr key={index}>
+        <td align='right'>{info.id}</td>
         <td>{info.name}</td>
         <td>{info.email}</td>
         <td>{info.address}</td>
@@ -18,25 +152,18 @@ function TableJson() {
 
   return (
     <div>
-      <table
-        loading={loading}
-        columns={columns}
-        dataSource={dataSource}
-        pagination={{ current: 3, pageSize: 15 }}
-        class='table table-striped'
-      >
-        <thead>
-          <tr>
-            <th>NO</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-          </tr>
-        </thead>
-        <tbody>{DisplayData}</tbody>
-      </table>
+      <Table columns={columns} dataSource={DisplayData} />;
+      {/* <thead>
+        <tr>
+          <th>No</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th>
+        </tr>
+      </thead> */}
+      {/* <tbody>{DisplayData}</tbody> */}
     </div>
   );
-}
+};
 
 export default TableJson;
